478ec05432b7e6f95f86788121560b179f5730a7
yash9491 <yaswanthduggirala@gmail.com>
2019-06-21 14:25:56 +0530

    New UI

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {PagerServiceService} from '../pager-service.service';
-import {Speedtomarket} from '../Questions';
+import { PagerServiceService } from '../pager-service.service';
+import { Speedtomarket } from '../Questions';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-calculator',
@@ -12,86 +13,86 @@ export class CalculatorComponent implements OnInit {
   public allItems: any;
   public pager: any = {}
   pagedItems: Speedtomarket[];
-  name: any="Devops";
+  name: any = "Devops";
   speedtomarket: Speedtomarket[];
 
-  constructor(private _pagerservice: PagerServiceService) {
+  constructor(private _pagerservice: PagerServiceService, private _router:Router) {
     this.speedtomarket = [
       {
-        QuestionId: 1, 
-        Question:'What is the Agile implemetation duration ',
+        QuestionId: 1,
+        Question: 'What is the Agile implemetation duration ',
         BefDevHr: 23.96,
         AftDevHr: 18.56,
         BefDevCst: 256.36,
         AftDevCst: 120.00
       },
       {
-        QuestionId: 2, 
-        Question:'What is Planning stage duration',
+        QuestionId: 2,
+        Question: 'What is Planning stage duration',
         BefDevHr: 42.58,
         AftDevHr: 22.23,
         BefDevCst: 52.12,
         AftDevCst: 95.63
       },
       {
-        QuestionId: 3, 
-        Question:'What is the Agile implemetation duration',
+        QuestionId: 3,
+        Question: 'What is the Agile implemetation duration',
         BefDevHr: 23.96,
         AftDevHr: 18.56,
         BefDevCst: 256.36,
         AftDevCst: 120.00
       },
       {
-        QuestionId: 4, 
-        Question:'What is Planning stage duration',
+        QuestionId: 4,
+        Question: 'What is Planning stage duration',
         BefDevHr: 42.58,
         AftDevHr: 22.23,
         BefDevCst: 52.12,
         AftDevCst: 95.63
       },
       {
-        QuestionId: 5, 
-        Question:'What is the Agile implemetation duration',
+        QuestionId: 5,
+        Question: 'What is the Agile implemetation duration',
         BefDevHr: 23.96,
         AftDevHr: 18.56,
         BefDevCst: 256.36,
         AftDevCst: 120.00
       },
       {
-        QuestionId: 6, 
-        Question:'tttttttttttttttttttttttttttttttttt',
-        BefDevHr: 23.96,
-        AftDevHr: 18.56,
-        BefDevCst: 256.36,
-        AftDevCst: 120.00
+        QuestionId: 6,
+        Question: 'tttttttttttttttttttttttttttttttttt',
+        BefDevHr: 20,
+        AftDevHr: 30,
+        BefDevCst: 56.36,
+        AftDevCst: 20.00
       },
       {
-        QuestionId: 7, 
-        Question:'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+        QuestionId: 7,
+        Question: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
         BefDevHr: 42.58,
         AftDevHr: 22.23,
         BefDevCst: 52.12,
         AftDevCst: 95.63
       },
       {
-        QuestionId: 8, 
-        Question:'yyyyyyyyyyyyyyyyyyyyyyyy',
-        BefDevHr: 23.96,
-        AftDevHr: 18.56,
-        BefDevCst: 256.36,
-        AftDevCst: 120.00
+        QuestionId: 8,
+        Question: 'yyyyyyyyyyyyyyyyyyyyyyyy',
+        BefDevHr: 52,
+        AftDevHr: 42.63,
+        BefDevCst: 55,
+        AftDevCst: 20.00
       },
       {
-        QuestionId: 9, 
-        Question:'hhhhhhhhhhhhhhhhhhhhhh',
-        BefDevHr: 42.58,
-        AftDevHr: 22.23,
-        BefDevCst: 52.12,
-        AftDevCst: 95.63
+        QuestionId: 9,
+        Question: 'hhhhhhhhhhhhhhhhhhhhhh',
+        BefDevHr: 24.63,
+        AftDevHr: 10.23,
+        BefDevCst: 55.23,
+        AftDevCst: 45.20
       },
       {
-        QuestionId: 10, 
-        Question:'ffffffffffffffffffffffffffffffff',
+        QuestionId: 10,
+        Question: 'ffffffffffffffffffffffffffffffff',
         BefDevHr: 23.96,
         AftDevHr: 18.56,
         BefDevCst: 256.36,
@@ -106,11 +107,14 @@ export class CalculatorComponent implements OnInit {
   }
 
   setPage(page: number) {
-  this.pager = this._pagerservice.getPager(this.allItems.length, page);
-  this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
-}
-submit(){
-  console.log(this.speedtomarket);
-}
+    this.pager = this._pagerservice.getPager(this.allItems.length, page);
+    this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
+  submit() {
+    console.log(this.speedtomarket);
+  }
+  gotoHome(){
+    this._router.navigateByUrl('');
+  }
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  
   constructor(private _router:Router) { }
 
   ngOnInit() {
   }
-
+  
   navigatetocalc(){
     this._router.navigateByUrl('/calculator');
   }
